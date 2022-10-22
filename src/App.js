9434662fc22ff34418507371d7a279644cc9270d
ryudai <bigdragon.06102001@gmail.com>
2022-10-23 03:36:01 +0900

    feat: :sparkles: 認証情報の追加
    google認証の追加。送信データにuidを追加。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import BrowseScreen from "./components/BrowseScreen";
 import Textarea from "./components/Textarea";
+import {
+  Auth,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import { provider } from "./firebase/firebase-config";
 
+export const UserContext = createContext();
 export const ContentContext = createContext();
 
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    alert("ログインして下さい");
+    signIn(auth, provider);
+  }
+});
+
+/**
+ *
+ * @param {Auth} auth
+ * @param {GoogleAuthProvider} provider
+ */
+const signIn = () => {
+  signInWithPopup(auth, provider).catch((error) => {
+    alert("ログインに失敗しました");
+    const errorMessage = error.message;
+    console.log(errorMessage);
+  });
+};
+
 function App() {
+  const [uid, setUid] = useState("");
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUid(user.uid);
+      }
+    });
+  }, []);
+
+  const userContextValue = {
+    uid,
+    signIn,
+  };
+
   const [content, setContent] = useState({
     id: "",
     title: "",
@@ -12,7 +56,7 @@ function App() {
     created_at: "",
   });
   const [canEditContent, setCanEditContent] = useState(true);
-  const value = {
+  const contentContextValue = {
     content,
     setContent,
     canEditContent,
@@ -20,8 +64,10 @@ function App() {
   };
 
   return (
-    <ContentContext.Provider value={value}>
-      {canEditContent ? <Textarea /> : <BrowseScreen />}
+    <ContentContext.Provider value={contentContextValue}>
+      <UserContext.Provider value={userContextValue}>
+        {canEditContent ? <Textarea /> : <BrowseScreen />}
+      </UserContext.Provider>
     </ContentContext.Provider>
   );
 }
